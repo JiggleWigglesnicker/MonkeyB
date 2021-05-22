a2219cd8a38d72fc940a4c82a80e2b1192f360f4
justin <games.j@live.nl>
2021-05-22 20:14:21 +0200

    Binded ViewModels to Views
    Binded ViewModels to Views

--- MonkeyB/Views/BuySellView.xaml.cs
+++ MonkeyB/Views/BuySellView.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MonkeyB.ViewModels;
 
 namespace MonkeyB.Views
 {
@@ -22,6 +23,7 @@ namespace MonkeyB.Views
         public BuySellView()
         {
             InitializeComponent();
+            this.DataContext = new BuySellViewModel();
         }
     }
 }
--- MonkeyB/Views/NewsView.xaml.cs
+++ MonkeyB/Views/NewsView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MonkeyB.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace MonkeyB.Views
         public NewsView()
         {
             InitializeComponent();
+            this.DataContext = new NewsViewModel();
         }
     }
 }
