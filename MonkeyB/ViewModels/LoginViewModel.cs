d7b6e8ae6cd3b8fc22c7a3c482e9a99219d84f8f
Thierry <degrootthierry@gmail.com>
2021-05-26 20:39:48 +0200

    Revert "MVVM structure / commands changed/added"
    This reverts commit ce902b705a79b8e6f75185cfcf187935676ca9e8.

--- MonkeyB/ViewModels/LoginViewModel.cs
+++ MonkeyB/ViewModels/LoginViewModel.cs
@@ -1,11 +1,8 @@
-﻿using Apex.MVVM;
-using MonkeyB.Views;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 
 namespace MonkeyB.ViewModels
 {
@@ -13,23 +10,8 @@ namespace MonkeyB.ViewModels
     {
         public ApiHandler api = new ApiHandler();
 
-        private Command loginCommand;
-        public Command LoginCommand
-        {
-            get { return loginCommand; }
-        }
-
-        public LoginViewModel(Window window)
-        {
+        public LoginViewModel() {
             api.GetApiData();
-
-            loginCommand = new Command(() =>
-            {
-                var dash = new DashboardView();
-                dash.Show();
-                window.Close();
-            });
-
         }
 
     }
