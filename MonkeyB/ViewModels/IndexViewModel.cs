ea472590c8035d3a2b7f07f53981cfa1ddc79fae
Stuxles <stuxles@live.nl>
2021-06-06 20:40:50 +0200

    first steps, add model

--- MonkeyB/ViewModels/IndexViewModel.cs
+++ MonkeyB/ViewModels/IndexViewModel.cs
@@ -1,20 +1,36 @@
 ﻿using MonkeyB.Commands;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Newtonsoft.Json;
 
 namespace MonkeyB.ViewModels
 {
     class IndexViewModel : BaseViewModel
     {
         public ICommand DashBoardCommand { get; set; }
+        
+        
 
         public IndexViewModel(NavigationStore navigationStore)
         {
             DashBoardCommand = new NavigateDashBoardCommand(navigationStore);
+            GetIndexData();
+        }
+        
+        public static readonly Uri BaseUri = new Uri("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=1392577232&to=1422577232");
+
+        
+        public MarketGraph marketmodel = new MarketGraph();
+        private void GetIndexData()
+        {
+
         }
     }
 }
