09b30f45a249dd6f22eded409f123010e66726f8
justin <games.j@live.nl>
2021-06-10 21:19:23 +0200

    Added RSSFeed
    Added RSSFeed

--- MonkeyB/ViewModels/DashBoardViewModel.cs
+++ MonkeyB/ViewModels/DashBoardViewModel.cs
@@ -1,10 +1,19 @@
 ﻿using MonkeyB.Commands;
+using MonkeyB.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
+using System.ServiceModel.Syndication;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
+using System.Xml;
 
 namespace MonkeyB.ViewModels
 {
@@ -15,13 +24,40 @@ namespace MonkeyB.ViewModels
         public ICommand NewsCommand { get; set; }
         public ICommand AchievementCommand { get; set; }
         public ICommand SettingCommand { get; set; }
+        public ObservableCollection<RSSModel> RSSList { get; set; }
+
+        //private string rSSTitle;
+        //public string RSSTitle
+        //{
+        //    get => rSSTitle;
+
+        //    set
+        //    {
+        //        rSSTitle = value;
+        //        OnPropertyChanged("RSSTitle");
+        //    }
+        //}
+
+        //private string rSSContent;
+        //public string RSSContent
+        //{
+        //    get => rSSContent;
+
+        //    set
+        //    {
+        //        rSSContent = value;
+        //        OnPropertyChanged("RSSContent");
+        //    }
+        //}
+
+
 
         public DashBoardViewModel(NavigationStore navigationStore)
         {
             IndexCommand = new RelayCommand(o =>
             {
                 navigationStore.SelectedViewModel = new IndexViewModel(navigationStore);
-            }); 
+            });
 
             BuySellCommand = new RelayCommand(o =>
             {
@@ -42,7 +78,44 @@ namespace MonkeyB.ViewModels
             {
                 navigationStore.SelectedViewModel = new SettingViewModel(navigationStore);
             });
+
+            RSSList = new ObservableCollection<RSSModel>();
+            ReadRSSNodes();
+
+
         }
+
+        public async void ReadRSSNodes()
+        {
+
+            await Task.Run(async () =>
+            {
+
+                string url = "https://www.nu.nl/rss/Economie";
+                XmlReader reader = XmlReader.Create(url);
+                SyndicationFeed feed = SyndicationFeed.Load(reader);
+                reader.Close();
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    String subject = item.Title.Text;
+                    String summary = item.Summary.Text;
+                    int index = summary.IndexOf("<");
+                    if (index >= 0)
+                        summary = summary.Substring(0, index);
+                    Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new ThreadStart(delegate
+                    {
+                        RSSList.Add(new RSSModel(subject, summary));
+                    }));
+
+                }
+
+            });
+
+
+        }
+
+
+
 
     }
 }
