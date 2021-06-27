fbe68baeded0c67d5e16597ac9634dccafd0a821
Thierry <degrootthierry@gmail.com>
2021-06-27 15:14:25 +0200

    changes to transactionHistorymodel
    added comments + made a separate function of the color being applied to profit or loss

--- MonkeyB/Models/TransactionHistoryModel.cs
+++ MonkeyB/Models/TransactionHistoryModel.cs
@@ -1,22 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MonkeyB.Models
+﻿namespace MonkeyB.Models
 {
     public class TransactionHistoryModel
     {
         public string coinName { get; set; }
         public float coinAmount { get; set; }
-        public float coinValue { get; set; }
+        public float coinValue { get; set; }            // the real-time value of a certain coin present in wallet
         public float coinPercentage { get; set; }
-        public float oldCoinValue { get; set; }
+        public float oldCoinValue { get; set; }         // the coinvalue as it was when it was purchased by the user
         public float profitLossValue { get; set; }
         public string percentageColor { get; set; }
 
-
+        /// <summary>
+        /// Large constructor of TransactionHistoryModel with 3 arguments
+        /// </summary>
+        /// <param name="coinName"></param>
+        /// <param name="coinAmount"></param>
+        /// <param name="oldCoinValue"></param>
         public TransactionHistoryModel(string coinName, float coinAmount, float oldCoinValue)
         {
             this.coinName = coinName;
@@ -24,38 +23,56 @@ namespace MonkeyB.Models
             this.oldCoinValue = oldCoinValue;
         }
 
+        /// <summary>
+        /// Small constructor of TransactionHistoryModel with 2 arguments
+        /// </summary>
+        /// <param name="coinName"></param>
+        /// <param name="coinAmount"></param>
         public TransactionHistoryModel(string coinName, float coinAmount)
         {
             this.coinName = coinName;
             this.coinAmount = coinAmount;
         }
 
+        /// <summary>
+        /// Calculates the percentage of the profit or loss with coinAmount
+        /// </summary>
         public void calculateProfitOrLoss()
         {
             profitLossValue = (oldCoinValue - coinValue) * coinAmount;
-            if (profitLossValue < 0 || profitLossValue < 0.0 || profitLossValue == 0)
-            {
-                percentageColor = "Red";
-            }
-            else
-            {
-                percentageColor = "Green";
-            }
+            
+            percentageColor = returnPercentageColor(coinPercentage);
 
+            percentageColor = returnPercentageColor(coinPercentage);
         }
 
+        /// <summary>
+        /// Calculates the percentage of the profit or loss with oldCoinValue
+        /// </summary>
         public void calculatePercentage()
         {
             coinPercentage = 100 * (coinValue - oldCoinValue) / oldCoinValue;
+
+            percentageColor = returnPercentageColor(coinPercentage);
+
+            percentageColor = returnPercentageColor(coinPercentage);
+        }
+
+        /// <summary>
+        /// Returns the right color to be given to the label of the profit or loss amount
+        /// </summary>
+        /// <param name="coinPercentage"></param>
+        /// <returns></returns>
+        public string returnPercentageColor(float coinPercentage)
+        {
             if (coinPercentage < 0 || coinPercentage < 0.0 || coinPercentage == 0)
             {
-                percentageColor = "Red";
+                return "Red";
             }
             else
             {
-                percentageColor = "Green";
+                return "Green";
             }
-            
         }
     }
 }
